7e81ee019055922667fd659c8548bbe993362638
Greg Daviet <Greg@theDaviets.com>
2011-09-05 11:31:34 -0600

    Need to change spec_helper to get spork working

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,5 @@
 class Company < ActiveRecord::Base
+
+  validates :name,  :presence => true,
+                    :length   => { :maximum => 50 }
 end
--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,5 +1,40 @@
 require 'spec_helper'
 
-describe Company do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe "Company" do
+
+  before(:each) do
+    @attr = {
+       :name => "John Doe Farms"
+    }
+
+  end
+  
+  it "should create a new instance given valid attributes" do
+    Company.create!(@attr)
+  end
+
+  describe "attributes" do
+
+    before(:each) do
+      @company = Company.new(@attr)
+    end
+
+    it "should include 'name'" do
+      @company.should respond_to(:name)
+    end
+  end
+
+  describe "validations" do
+
+
+    it "should require a name" do
+      no_name_company = Company.new(@attr.merge(:name => ""))
+      no_name_company.should_not be_valid
+    end
+
+    it "should not allow long names" do
+      long_name_company = Company.new(@attr.merge(:name => "a" * 51))
+      long_name_company.should_not be_valid
+    end
+  end
 end
