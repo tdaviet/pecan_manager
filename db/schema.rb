e0d6dc326b8f99a007c05dd2ec40d220e663a48f
Greg Daviet <Greg@theDaviets.com>
2011-09-05 09:12:06 -0600

    Created companies migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110904031906) do
+ActiveRecord::Schema.define(:version => 20110905045751) do
+
+  create_table "companies", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "companies", ["name"], :name => "index_companies_on_name", :unique => true
 
   create_table "users", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false
