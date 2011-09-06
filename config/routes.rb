0995e4687dfae4ca20ee4eee58d6dc121bc480f7
Theresa Daviet <Theresa@theDaviets.com>
2011-09-06 15:45:52 -0600

    layout changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 PecanManager::Application.routes.draw do
-  get "pages/home"
-  get "pages/contact"
-  get "pages/about"
-  get "pages/help"
-
-  devise_for :users
+  
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+  
+  root :to => "pages#home"
+  
+#  devise_for :users
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -55,7 +57,7 @@ PecanManager::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "pages#home"
+
 
   # See how all your routes lay out with "rake routes"
 
